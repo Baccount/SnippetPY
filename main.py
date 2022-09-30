8f82fd0c57b33788c295215af2bb112c96e20314
“Brandon <cassias.long0r@icloud.com>
2022-09-30 15:05:21 -0700

    many new options

--- main.py
+++ main.py
@@ -1,21 +1,59 @@
 # keep a saved list of code snippets for later use
 
-
+from os import path
 import tkinter as tk
-
+import sqlite3
 
 
 # Add a new snippet
 def add_snippet():
-    # add each line of text to a list
+    # add all text to a variable
     txt = text.get("1.0", "end-1c")
-    lines = txt.splitlines()
-    print(lines)
+    # save the text
+    save_snippet(txt)
+
+def delete_snippet():
+    # delete the selected snippet in the listbox
+    listbox.delete("active")
+    # delete the snippet from the database
+    conn = sqlite3.connect("snippets.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM snippets WHERE snippet = ?", (listbox.get("active"),))
+    conn.commit()
+    conn.close()
+
+# load all snippets from the database and show them in the listbox
+def load_snippets():
+    # check if the database exists
+    if path.isfile("snippets.db"):
+        # load the snippets from the database
+        conn = sqlite3.connect("snippets.db")
+        c = conn.cursor()
+        c.execute("SELECT * FROM snippets")
+        snippets = c.fetchall()
+        # show the snippets in the listbox
+        for snippet in snippets:
+            listbox.insert("end", snippet[0])
+        conn.close()
 
 
-
-
-
+def save_snippet(text):
+    # save the text using SQLite database
+    # create a new database if one doesn't exist
+    if not path.isfile("snippets.db"):
+        conn = sqlite3.connect("snippets.db")
+        c = conn.cursor()
+        c.execute('''CREATE TABLE snippets
+                    (snippet text)''')
+        conn.commit()
+        conn.close()
+    # add the snippet to the database
+    conn = sqlite3.connect("snippets.db")
+    c = conn.cursor()
+    c.execute("INSERT INTO snippets VALUES (?)", (text,))
+    conn.commit()
+    
+    
 
 
 
@@ -23,21 +61,34 @@ def add_snippet():
 
 window = tk.Tk()
 window.title("Code Snippets")
-window.geometry("500x500")
+window.geometry("600x600")
 window.resizable(False, False)
 # add button to add new snippet
 # add button to delete snippet
 button = tk.Button(window, text="Add Snippet", command=add_snippet)
 button.pack()
-button = tk.Button(window, text="Delete Snippet")
+button = tk.Button(window, text="Delete Snippet", command=delete_snippet)
 button.pack()
 # add text box to show snippet
 text = tk.Text(window, height=10, width=50)
 text.pack()
+# add listbox to show saved snippets and scrollbar allow copy and paste
+listbox = tk.Listbox(window, height=10, width=50, yscrollcommand=True, selectmode="single")
+# place on bottom of window
+listbox.pack(side="bottom")
+
+
+
 
 
 def main():
-    window.mainloop()
+    try: 
+        load_snippets()
+        window.mainloop()
+        
+    except Exception as e:
+        print(e)
+        print("Error: unable to start thread")
 
 
 
